3115f5a89573210cda47f6629123e84f715116af
hagikehappy <hagikehappy@163.com>
2024-10-27 14:34:06 +0800

    Upload

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(file) -> list:
 
 setup(
     name='hagike',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=read('requirements.txt'),
     include_package_data=True,
